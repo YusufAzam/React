de643a0259d4fb734129d88d90636dfebb226729
Yusuf Azam <yusufazam95@hotmail.co.uk>
2021-09-01 16:38:11 +0100

    adding second component

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,17 +1,25 @@
+//Import React & React Dom libraries
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom'
 
+
+const getButtonText = ()=> "Click on me!"
+//Create a react component
+const App = () => {
+    const buttonText = "Click me";
+    return (
+        <div>
+            <label className="label" htmlFor="name">
+                Enter name:
+            </label>
+            <input id="name" type="text"/>
+            <button style={{backgroundColor: 'blue', color: 'white'}}>{buttonText}</button>
+        </div>
+    )
+}
+
+// Take the react component and show on screen
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />,
+    document.querySelector('#root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
